1aaa566f05a0e9c481a257ed7d2360e11545a519
tonnidiaz <therealhackingknight@gmail.com>
2023-10-06 07:33:16 +0200

    add vars.zip

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ if [ $1 = "sync" ]
     else
         echo SYNC, BUILD AND SIGN
         ionic cap sync android && cd android && ./gradlew assembleRelease && cd ../android/app/build/outputs/apk/release && jarsigner -keystore /home/tonni/Desktop/Keys/ionic-key.jks -storepass Baseline@072 app-release-unsigned.apk key0 && zipalign 4 app-release-unsigned.apk app-release.apk
-        echo /android/app/build/outputs/apk/release/app-release.apk
+        echo xdg-open ./android/app/build/outputs/apk/release/
         echo DONE!
 fi
 #
--- todo.md
+++ todo.md
@@ -11,10 +11,13 @@
 - show only approved reviews
 - filter reviews
 - One rev per product per person
+- dark btn on edit rev sheet
+- del acc alert to ios
 ## FIXES
-
-- Store details
+- Admin dash products count
 - chekout flicker after add address
+- Loader for adding user res address
+- admin cannot go to user acc settings
 
 # FUTURE FEATURES
 
