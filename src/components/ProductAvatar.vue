80f76462a5e1a487529aa29cf9487208fe0bffd1
tonnidiaz <therealhackingknight@gmail.com>
2023-10-08 23:43:27 +0200

    better product avatar

---
+++ src/components/ProductAvatar.vue
@@ -0,0 +1,80 @@
+<template>
+<router-link
+                          
+                            :to="`/product/${item.pid}`"
+                            class="flex flex-col items-center flex-shrink-0 w-90px bg- relative overflow-hidden ion-activatable"
+                        >
+                        <ion-ripple-effect/>
+                            <div
+                                class="flex flex-col w-full items-center overflow-hidden"
+                            >
+                                <div class="avatar">
+                                    <div
+                                        style="display: flex !important"
+                                        class="w-80px rounded-full bg-base-100 flex items-center justify-center"
+                                    >
+                                        <img
+                                            v-if="item.images?.length"
+                                            alt=""
+                                            :src="item.images[0].url"
+                                        />
+
+                                        <span v-else>
+                                            <i class="fi fi-rr-image-slash"></i>
+                                        </span>
+                                    </div>
+                                </div>
+
+                                
+                                <div v-if="sale">
+                                    <h5 class="mt-2 text-black fs-12 fw-6 linethrough">
+                                    R{{ item.price.toFixed(2) }}
+                                </h5>
+                                    <h5
+                                class="text-black fs-12 fw-6"
+                                style="transform: scale(1.2)"
+                            >
+                                R{{ item.sale_price.toFixed(2) }}
+                            </h5>
+                                </div>
+                                <h5 v-else class="mt-2 text-black fs-12 fw-6">
+                                    R{{ item.price.toFixed(2) }}
+                                </h5>
+                                <div class="flex items-start justify-start overflow-hidden w-full">
+                                    <h4
+                                        class="fs-14 fw-5 overflow-hidden"
+                                        style="
+                                            white-space: nowrap;
+                                            text-overflow: ellipsis;
+                                        "
+                                    >
+                                        {{ item.name }} Lorem ipsum, dolor sit amet
+                                        consectetur adipisicing elit.
+                                        Consectetur voluptatem quam et
+                                        voluptates provident earum, quas debitis
+                                        eos dolorem praesentium! Totam fugit
+                                        dignissimos ipsa ad omnis porro impedit
+                                        exercitationem obcaecati.
+                                    </h4>
+                                </div>
+                            </div>
+                        </router-link>
+</template>
+<script setup lang="ts">
+import {
+    IonPage,
+    IonContent,
+    IonSearchbar,
+    IonText,
+    IonRefresher,
+    IonRefresherContent,
+    IonRippleEffect
+} from "@ionic/vue";
+import { Obj } from '@/utils/classes';
+
+ 
+defineProps<{
+    item: Obj,
+    sale?: boolean
+}>()
+</script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,6 @@
 <template>
     <ion-page>
-       <appbar :show-back="false"/>
+        <appbar :show-back="false" />
         <ion-content :fullscreen="true">
             <ion-refresher slot="fixed" @ion-refresh="onRefresh">
                 <ion-refresher-content />
@@ -10,103 +10,32 @@
                 <IonText class="text-xl my-2 font-pacifico fs-30 fw-8"
                     >Grab yours now!</IonText
                 >
-                <ion-searchbar router-link="/search" class="rounded"></ion-searchbar>
+                <ion-searchbar
+                    router-link="/search"
+                    class="rounded"
+                ></ion-searchbar>
                 <div class="my-2" v-if="topSelling">
                     <!-- Top selling section -->
                     <h3 class="fs-20 fw-5 my-3">Top selling</h3>
-                    <div class="mt-2 flex overflow-scroll">
-                        <InkWell
-                            v-if="topSelling"
-                            @click="() => router.push(`/product/${e.pid}`)"
-                            v-for="(e, i) in topSelling"
-                            class="flex flex-col items-center flex-shrink-0"
-                        >
-                            <div class="flex flex-col items-center">
-                                <div class="avatar">
-                                    <div style="display: flex !important;"
-                                    class="w-80px rounded-full bg-base-100 flex items-center justify-center"
-                                >
-                                    <img
-                                        v-if="e.images?.length"
-                                        alt=""
-                                        :src="e.images[0].url"
-                                    />
-
-                                    <span v-else>
-                                        <i class="fi fi-rr-image-slash"></i>
-                                    </span>
-                                    </div>
-                                </div>
-                                <h5 class="mt-2 text-black fs-12 fw-6">
-                                    R{{ e.price.toFixed(2) }}
-                                </h5>
-                                <h4 class="fs-14 fw-5">{{ e.name }}</h4>
-                            </div>
-                        </InkWell>
+                    <div class="mt-2 flex overflow-scroll gap-x-3">
+                       <ProductAvatar  v-if="topSelling"
+                            v-for="(e, i) in topSelling" :item="e"/>
                     </div>
                 </div>
                 <div class="my-2" v-if="special">
                     <!-- Special section -->
                     <h3 class="fs-20 fw-5 my-3">Today's special</h3>
-                    <div class="mt-2 flex gap- overflow-scroll">
-                        <InkWell
-                            @click="() => router.push(`/product/${e.pid}`)"
-                            v-if="special"
-                            v-for="(e, i) in special"
-                            class="flex flex-col items-center flex-shrink-0"
-                        >
-                            <div class="avatar">
-                                <div style="display: flex !important;"
-                                    class="w-80px rounded-full bg-base-100 flex items-center justify-center"
-                                >
-                                    <img
-                                        v-if="e.images?.length"
-                                        alt=""
-                                        :src="e.images[0].url"
-                                    />
-
-                                    <span v-else>
-                                        <i class="fi fi-rr-image-slash"></i>
-                                    </span>
-                                </div>
-                            </div>
-                            <h5 class="mt-2 text-black fs-12 fw-6">
-                                R{{ e.price.toFixed(2) }}
-                            </h5>
-                        </InkWell>
+                    <div class="mt-2 flex gap-x-3 overflow-scroll">
+                       <ProductAvatar  v-if="special"
+                            v-for="(e, i) in special" :item="e"/>
                     </div>
                 </div>
                 <div class="my-2" v-if="sale">
                     <!-- Special section -->
                     <h3 class="fs-20 fw-5 my-3">On sale</h3>
-                    <div class="mt-2 flex gap- overflow-scroll">
-                        <InkWell
-                            @click="() => router.push(`/product/${e.pid}`)"
-                            v-for="(e, i) in sale"
-                            class="flex flex-col items-center flex-shrink-0"
-                        >
-                            <div class="avatar">
-                                <div style="display: flex !important;"
-                                    class="w-80px rounded-full bg-base-100 flex items-center justify-center"
-                                >
-                                    <img
-                                        v-if="e.images?.length"
-                                        alt=""
-                                        :src="e.images[0].url"
-                                    />
-
-                                    <span v-else>
-                                        <i class="fi fi-rr-image-slash"></i>
-                                    </span>
-                                </div>
-                            </div>
-                            <h5 class="mt-2 text-black fs-12 fw-5 linethrough">
-                                R{{ e.price.toFixed(2) }}
-                            </h5>
-                            <h5 class="text-black fs-12 fw-6" style="transform: scale(1.2);">
-                                R{{ e.price.toFixed(2) }}
-                            </h5>
-                        </InkWell>
+                    <div class="mt-2 flex gap-x-3 overflow-scroll">
+                        <ProductAvatar  v-if="sale"
+                            v-for="(e, i) in sale" :item="e" sale/>
                     </div>
                 </div>
             </div>
@@ -122,15 +51,17 @@ import {
     IonText,
     IonRefresher,
     IonRefresherContent,
+    IonRippleEffect
 } from "@ionic/vue";
 import { onMounted, ref } from "vue";
 import InkWell from "@/components/InkWell.vue";
 import { useRouter } from "vue-router";
 import axios from "axios";
 import { apiURL } from "@/utils/constants";
+import ProductAvatar from "@/components/ProductAvatar.vue";
 const special = ref<any[]>(),
     topSelling = ref<any[]>(),
-    sale = ref<any[]>()
+    sale = ref<any[]>();
 const router = useRouter();
 
 const getProducts = async (q: string) => {
